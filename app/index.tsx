2444bc40765a7f05af85bcc64530d455a2335a42
Silvio Ceccarini <s.ceccarini94@gmail.com>
2023-09-05 22:08:21 +0200

    try to add push notification

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,22 @@
 import { Redirect } from "expo-router";
+import messaging from "@react-native-firebase/messaging";
+import { useEffect } from "react";
 
 export default function Index() {
+  const requestPermissionUser = async () => {
+    console.log("yes");
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  };
+  useEffect(() => {
+    (async () => await requestPermissionUser())();
+  }, []);
   // return <Redirect href="/home/pokedex-js" />;
   return <Redirect href="/home" />;
 }
